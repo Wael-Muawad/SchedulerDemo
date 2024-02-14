a93c6a84afb573c22196c7119e75a917f742d26d
WaelMuawad <WaelMuawad@WAEL>
2024-02-14 15:09:38 +0300

    Add project files.

---
+++ SchedulerDemo/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SchedulerDemo;
+//using Syncfusion.Blazor;
+
+
+
+//Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NAaF1cXmhKYVJyWmFZfVpgcl9DY1ZVTWYuP1ZhSXxXdkdiWn9ZdX1XQGZbU00=");
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddSyncfusionBlazor();
+ 
+
+
+await builder.Build().RunAsync();
